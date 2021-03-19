ac521018860b79e6d880d21437087a5c1aaa0b0b
asa-windows <main@asa-prog.com>
2021-03-19 15:46:22 +0900

    add:卒業論文のサイトにアクセスしてpdfが見れるか確認

--- app/src/main/java/com/asa/webview_test/MainActivity.kt
+++ app/src/main/java/com/asa/webview_test/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        webView.loadUrl("https://www.google.com/")
+        webView.loadUrl("https://shiobaraseminar.jimdofree.com/works/2017%E5%B9%B4%E5%BA%A6%E5%8D%92%E6%A5%AD%E7%94%9F-%E5%8D%92%E6%A5%AD%E8%AB%96%E6%96%87%E4%B8%80%E8%A6%A7/")
     }
 }
